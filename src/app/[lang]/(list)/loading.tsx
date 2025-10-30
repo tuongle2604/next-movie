2497db7025d3c7437f34b42fbbda99081ac64ebb
DESKTOP-KPPSS6H\tuong <tuongle2604@gmail.com>
2025-10-30 21:26:13 +0700

    reup

--- src/app/[lang]/(list)/loading.tsx
+++ src/app/[lang]/(list)/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="px-4">
       <div className="pt-3">
-        <Skeleton className="w-40 mb-5 h-7 md:mb-8 md:w-60 lg:h-9" />
+        <Skeleton className="mb-5 h-7 w-40 md:mb-8 md:w-60 lg:h-9" />
 
         <MovieCollectionsLoading />
       </div>
