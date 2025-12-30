04c0ada9e73037eb6e545af574cdfd1d27c85a18
DESKTOP-KPPSS6H\ASUS <tuongle2604@gmail.com>
2025-12-31 02:04:42 +0700

    reup

--- src/components/ui/next-image.tsx
+++ src/components/ui/next-image.tsx
@@ -3,10 +3,15 @@ import Image, { ImageProps } from "next/image";
 interface NextImageProps extends ImageProps {
   original?: boolean;
 }
+
 export default function NextImage({ src, original, ...props }: NextImageProps) {
+  const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+
+  console.log("IMAGE_BASE_URL ", IMAGE_BASE_URL);
+
   const BASE_URL = original
-    ? `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/original`
-    : `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/w500`;
+    ? `${IMAGE_BASE_URL}/original`
+    : `${IMAGE_BASE_URL}/w500`;
   // src =
 
   return <Image src={BASE_URL + src} {...props} />;
